11b8a3f5e4438a40ee56b1ffd945a30c40079e6b
HR Mandiv <hmandiv@outlook.com>
2021-02-06 19:53:49 +0000

    added support for multiple deletes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
   // inputs
   const edis = [
     {documentTypeKey: 4,  description: 'ASN' },
-    { documentTypeKey: 1, description: 'Invoice' },
+    {documentTypeKey: 1, description: 'Invoice' },
     {documentTypeKey: 5,  description: 'Claim' },
   ].map((doc)=> ({value: doc.documentTypeKey, label: doc.description}));
 
@@ -17,8 +17,8 @@
     { documentTypeKey: 6, description: 'Remittance Advice' }
   ].map((doc)=> ({value: doc.documentTypeKey, label: doc.description}));
 
-  const addEdi = { value: 2, label: 'PO' };
-  const deleteEdi = { value: 4, label: 'ASN' }; 
+  const addEdi = { value: 6, label: 'Remittance Advice' };
+  const deleteEdi = [{ value: 6, label: 'Remittance Advice' },{value: 2,  label: 'PO' }]; 
   const editEdi = { value: 1, label: 'Invoice' }; 
 
   // derived from edis 
@@ -31,10 +31,27 @@
       // returns true for document type that does not exist in existingDocumentTypes
       existingDocumentTypes.filter(
         (existingDocumentType) =>
-          documentType.label === existingDocumentType.label
+          documentType.value === existingDocumentType.value
       ).length === 0
   );
   console.log("Initial Available Document Types:- \n",getAvailableDocumentTypes(),"\n")
+
+  const checkIfDocumentIsAvailable = (existingDocumentType) =>
+    getAvailableDocumentTypes().some(
+      (availableDocumentType) => availableDocumentType.value === existingDocumentType.value
+    );
+  
+  const edit = (currentDocumentType, newDocumentType) => {
+    !checkIfDocumentIsAvailable(currentDocumentType) &&
+     checkIfDocumentIsAvailable(newDocumentType)
+      ? existingDocumentTypes = [
+          ...existingDocumentTypes.filter(
+            (doc) => doc.value !== currentDocumentType.value
+          ),
+          newDocumentType,
+        ]
+      : existingDocumentTypes;
+  }
 
   const saveEDI = () => existingDocumentTypes.map((doc)=>({documentTypeKey: doc.value, description: doc.label}));
 /////////////////////////////////////////////////////////////////////////////////////
@@ -50,11 +67,6 @@
   console.log(`${JSON.stringify(addEdi)}\n`)
   console.log(".....................................................................\n")
 
-  const checkIfDocumentIsAvailable = (existingDocumentType) =>
-    getAvailableDocumentTypes().some(
-      (availableDocumentType) => availableDocumentType.label === existingDocumentType.label
-    );
-
   console.log(`USER SHOULD BE ABLE TO ADD: ${checkIfDocumentIsAvailable(addEdi)}`,"\n")
 
   existingDocumentTypes = checkIfDocumentIsAvailable(addEdi) ? [
@@ -69,25 +81,14 @@
 /////////////////////////////////////////////////////////////////////////////////////
                                 //EDIT\\
 /////////////////////////////////////////////////////////////////////////////////////
-
+  // have to figure ux flow and control as to how a user will edit
+  // this is just to check how the data can be switched automatically
   // user edits a document type
   console.log(".....................................................................\n")
   console.log("USER EDITS A DOCUMENT TYPE\n")
   console.log(`Replaces ${JSON.stringify(editEdi)}\n`)
   console.log(`Adds ${JSON.stringify(getAvailableDocumentTypes()[0])}\n`)
   console.log(".....................................................................\n")
-
-  const edit = (currentDocumentType, newDocumentType) => {
-    !checkIfDocumentIsAvailable(currentDocumentType) &&
-    checkIfDocumentIsAvailable(newDocumentType)
-      ? existingDocumentTypes = [
-          ...existingDocumentTypes.filter(
-            (doc) => doc.value !== currentDocumentType.value
-          ),
-          newDocumentType,
-        ]
-      : existingDocumentTypes;
-  }
 
   edit(editEdi, getAvailableDocumentTypes()[0]);
 
@@ -101,15 +102,26 @@
 
   // ********************** ADD Support for multiple deletes **********************
 
+  // have to figure ux flow and control as to how a user will delete
+  // this is just to check how the data can shrink automatically
   // user removes a document type
   // this removed document type should be available
   console.log(".....................................................................\n")
-  console.log("USER REMOVES A NEW DOCUMENT TYPE\n")
+  console.log("USER REMOVES DOCUMENT TYPE/s\n")
   console.log(`${JSON.stringify(deleteEdi)}\n`)
   console.log(".....................................................................\n")
 
   // remove a single document from existingDocumentTypes
-  existingDocumentTypes = existingDocumentTypes.filter((doc) => doc.value !== deleteEdi.value);
+    // existingDocumentTypes = existingDocumentTypes.filter((doc) => doc.value !== deleteEdi.value);
+  // remove multiple documentTypes
+    existingDocumentTypes = existingDocumentTypes.filter(
+      (existingDocumentType) =>
+        // returns true for document type that does not exist in existingDocumentTypes
+        deleteEdi.filter(
+          (documentTypesToDelete) =>
+            existingDocumentType.value === documentTypesToDelete.value
+        ).length === 0
+    );
   // add document to availableDocumentTypes ?
 
   console.log("New Existing Document Types:- \n",existingDocumentTypes,"\n")
